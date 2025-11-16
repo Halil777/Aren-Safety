ef83f36438d846d74f1efe485eeaa6f420d34feb
Lock <microsoft7779@gmail.com>
2025-11-16 17:46:20 +0300

    commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       "http://127.0.0.1:5173",
       "http://localhost:8080",
       "http://127.0.0.1:3000",
+      "http://80.78.243.218:7777", // Production frontend
     ],
     credentials: true,
   });

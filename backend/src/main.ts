2448caccf6a280c286718dbc62540bd37aa66589
Halil777 <microsoft7779@gmail.com>
2025-12-06 16:45:15 +0300

    update the cross origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,13 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://localhost:8080",
-      "http://127.0.0.1:3000",
-      "http://80.78.243.218:7777",
-    ],
+    origin: "*",
     credentials: true,
   });
 
